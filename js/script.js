0f9fdd73f06b68020b712dc1e6ebcd51df9492ea
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-01-09 09:56:51 +0100

    Tests JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var myString = "Hello you" ;
+
+var myNewString = myString.replace("you","me");
+
+console.log(myNewString);
+
+
+var string1 = "hello";
+
+var string2 = "you";
+
+
+var myNewString = string1 + " " + string2;
+
+
+console.log(myNewString);
