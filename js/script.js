30892fe6c5386d2181da92ec5dba70f7cf2d5690
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-01-27 18:37:33 +0100

    Création Canvas + refresh  Canvas

--- js/script.js
+++ js/script.js
@@ -1,229 +1,31 @@
-/*
+ window.onload = function()
+ {
+    var canvas;
+    var ctx;
+    var delay = 100;
+    var xCoord = 0;
+    var yCoord = 0;
 
-booleans
+    init();
 
-
-var x = 5;
-var y = 12;
-
-console.log(true && true)   et et
-console.log(true && false)
-console.log(false && true)
-console.log(false && false)
-
-console.log(true || true)   ou ou
-console.log(true || false)
-console.log(false || true)
-console.log(false || false)
-
-console.log(!true)
-console.log(!false)
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*CONDITIONS*/
-/*
-if(Condition1)
-{
-    console.log("execute ce code")
-}
-
-
-else
-{
-    console.log('alors execute ce code là!')
-}
-
-else if(condition2)
-{
-    console.log("lacondition1 est false mais la 2 l'est")
-}
-
-*/
-
-/*
-var speed = 30
-
-if(speed<80)
-{
-    if(speed<50)
+    function init()
     {
-        console.log('accelere un peu');
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+        document.body.appendChild(canvas); 
+        ctx = canvas.getContext('2d');
+        refreshCanvas();    
     }
-    else
-    {
-        console.log('tu roules à la bonne vitesse');
+    
+    function refreshCanvas()
+    {     
+        xCoord += 5;
+        yCoord += 5;
+        ctx.clearRect(0,0,canvas.width, canvas.height);      
+        ctx.fillStyle = '#ff0000';        
+        ctx.fillRect(xCoord, yCoord, 100, 50);
+        setTimeout(refreshCanvas, delay);
     }
-}
-
-
-else if(speed<100)
-{
-    console.log("ralentis un peu");
-}
-
-else
-{
-    console.log("tu roules trop vite");
-}
-*/
-
-
-
-
-
-
-
-
-
-/*syntaxe du switch
-
-switch(variable)
-{
-    case 1:
-        break;
-    case 2:
-        break;
-
-    default:
-
-}
-
-*/
-
-/*
-var favotitecolor = "red" ;
-
-switch(favotitecolor)
-    {
-        case "blue":
-            console.log("wow bleu trop bien")
-            break;
-        case "red":
-            console.log('je veux du rouge')
-        break;
-        default:
-            console.log('je sais pas quelle couleur')
-    }
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* boucles, tantque , alors */
-/*
-
-while permet de metre une condition tantque
-
-var number = 0
-while(number<3)
-{
-    console.log(number);
-    number++
-}
-*/
-
-/* do while permet de commencer à faire avant la condition 
-
-var number= 0;
-do{
-    console.log(number);
-    number++;
-}
-while(number<3)
-
-*/
-
-/* for en plusieurs parties   premiere etape initialiseer la variable, 2eme etape condition, 3eme etape une fois condition verifiée, executer le code et ensuite la partie 3, puis revérifie la condition */
-
-/*
-for(var number = 0; number < 5; number++)
-{
-    console.log(number)
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Fonctions */
-/*
-function name(parametres)
-{
-    code à executer
-    return Result;
-}
-*/
-/* déclarer une fonction */
-
-function multiply(number1,number2)
-{
-    return number1*number2
-}
-
-/* execution de la function */
-
-
-var a = 5;
-var b = 6;
-
-var result = multiply(a,b);
-
-console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ }
